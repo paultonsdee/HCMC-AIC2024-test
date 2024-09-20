12a8db89efcf0a5202bd77c334509c3f56b78156
MinLee0210 <bastreet124@gmail.com>
2024-09-20 21:43:10 +0700

    Adding script for downloading data ...

--- bash/clean_docker.sh
+++ bash/clean_docker.sh
@@ -2,6 +2,7 @@
 
 echo "Cleaning docker images ..."
 if docker image prune -f; then
+  docker rmi -f $(docker images -aq)
   echo "Successfully removed unused images."
 else
   echo "Error removing images."
@@ -9,6 +10,7 @@ fi
 
 echo "Cleaning docker containers ..."
 if docker container prune -f; then
+  docker rm -vf $(docker ps -aq)
   echo "Successfully removed stopped containers."
 else
   echo "Error removing containers."
