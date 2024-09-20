6ac90f7b2e823ad41726d2627f1e94d323777631
MinLee0210 <bastreet124@gmail.com>
2024-09-20 13:53:35 +0700

    Download dataset to cloud and add script to download the dataset using curl in

---
+++ db/dowload_file.sh
@@ -0,0 +1,6 @@
+# Download dataset file
+echo "Start downloading and extracting files .... "
+
+curl -L "https://drive.usercontent.google.com/download?id=18Fx1dmi_OJVdGE5ObU8NR08z7DNjCd9n&export=download&confirm=t" -o "dataset.zip"
+
+echo "Done!"
--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
+# Download data
+echo "Downloading data ..."
+cd /db
+bash download_file.sh
+cd ../
+
+# Run backend
+echo "Moving to backend, then run backend"
 cd /backend
 uvicorn main:app --host=0.0.0.0 --port=8000 --reload
