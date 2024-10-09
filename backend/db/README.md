0cd0cb22dd5637601929ec0ff891bdbaba723c46
MinLee0210 <bastreet124@gmail.com>
2024-10-09 17:11:12 +0700

    Add downloading script

--- backend/db/README.md
+++ backend/db/README.md
@@ -12,3 +12,9 @@ We support 2 versions of downloading dataset via command shell, via Google Drive
 
 1. Google Drive: This version got a problem that Google may prevent too many download request for a large file. 
 2. Kaggle: This version require `kaggle.json`, which is Kaggle's beta API, you can interact with Competitions and Datasets to download data, make submissions, and more via the command line. You can get it from [here](https://www.kaggle.com/settings),`https://www.kaggle.com/settings` , and store it in this directory.
+
+### Detail implementation for `kaggle` downloading
+
+1. Get API from `https://www.kaggle.com/settings`. It will give a `.json` file. 
+2. You will move that `.json` file into this directory, `/backend/db`. 
+3. Running downloading script via `bash download_file_from_kaggle.sh`.
--- backend/db/download_file_from_kaggle.sh
+++ backend/db/download_file_from_kaggle.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-# Load the JSON file
-JSON_FILE="kaggle.json"
-if [ ! -f "$JSON_FILE" ]; then
-  echo "Error: $JSON_FILE not found."
-  exit 1
+
+# Check if keyframes.csv exists, skip download if found
+if [ ! -f "keyframes.csv" ]; then
+  echo "Downloading and extracting files .... "
+
+  # Load the JSON file
+  JSON_FILE="kaggle.json"
+  if [ ! -f "$JSON_FILE" ]; then
+    echo "Error: kaggle.json not found."
+    exit 1
+  fi
+
+  # Load the JSON data and set the environment variables
+  
+  KAGGLE_USERNAME=$(jq -r '.username' "$JSON_FILE")
+  KAGGLE_KEY=$(jq -r '.key' "$JSON_FILE")
+
+  # # Install `kaggle` Python SDK
+  # python -m pip install kaggle || {
+  #   echo "Error installing kaggle SDK. Check your pip installation."
+  #   exit 1
+  # }
+
+  # # Download dataset
+  # kaggle datasets download pyetsvu/aic2024-extracted-data --path ./ --unzip || {
+  #   echo "Error downloading dataset. Check your kaggle credentials and network connection."
+  #   exit 1
+  # }
+
+  # # Remove .zip file
+  # rm -f ./aic2024-extracted-data.zip || {
+  #   echo "Error removing .zip file. Check your file system permissions."
+  #   exit 1
+  # }
+
+  # # Uninstall kaggle SDK
+  # python -m pip uninstall kaggle || {
+  #   echo "Error uninstalling kaggle SDK. Check your pip installation."
+  #   exit 1
+  # }
+  echo $KAGGLE_USERNAME
+  echo $KAGGLE_KEY
 fi
-
-# Load the JSON data and set the environment variables
-KAGGLE_USERNAME=$(jq -r '.username' "$JSON_FILE")
-KAGGLE_KEY=$(jq -r '.key' "$JSON_FILE")
-
-# Install `kaggle` Python SDK for downloading dataset via kaggle command.
-# Install kaggle SDK
-pip install kaggle || {
-  echo "Error installing kaggle SDK. Check your pip installation."
-  exit 1
-}
-
-# Download dataset
-kaggle datasets download pyetsvu/aic2024-extracted-data --path ./ --unzip || {
-  echo "Error downloading dataset. Check your kaggle credentials and network connection."
-  exit 1
-}
-
-# Remove .zip file
-rm -f ./aic2024-extracted-data.zip || {
-  echo "Error removing .zip file. Check your file system permissions."
-  exit 1
-}
-
-# Uninstall kaggle SDK
-pip uninstall kaggle || {
-  echo "Error uninstalling kaggle SDK. Check your pip installation."
-  exit 1
-}
